cb212e298137cc2d4ccb2d7d3d3ccac5023273ef
MilesGDKing <mdolphyking35@gmail.com>
2023-09-27 11:52:37 -0600

    Assignment 2 in progress

---
+++ app/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return (
+        <div>
+            <Link href="https://github.com/MilesGDKing/cprg306-assignments">GitHub Repository</Link>
+        </div>
+    );
+}
+
---
+++ app/week2/page.js
@@ -0,0 +1,15 @@
+import StudentInfo from "../student-info";
+
+
+export default function Page() {
+    return (
+        <main>
+            <StudentInfo />
+            <h1>Shopping list</h1>
+            <p>Milk</p> 
+            <p>Cheese</p> 
+            <p>Hamburger</p>
+
+        </main>
+        );
+}
